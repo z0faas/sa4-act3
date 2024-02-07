aff51c7359c71d613fcbc921326e13d82e546f43
Jonathan Maronde <j2mike4@gmail.com>
2024-02-07 13:28:29 -0600

    Modified guess_number.py to limit the amount of guesses the user has to guess the right number

--- guess_number.py
+++ guess_number.py
@@ -5,8 +5,10 @@ Number guessing game
 number = 10
 
 print("I'm thinking of a number...")
-guess = input("What number am I thinking of? Enter q to give up. ")
+print('Beware! There are a limited amount of guesses!')
+guess = input("What number am I thinking of? Enter q if you give up. ")
 
+guess_amount = 5
 again = True
 while again:
 
@@ -19,5 +21,19 @@ while again:
         again = False
 
     else:
-        print(f"Sorry! That's the wrong number. Try again! ")
-        guess = input("What number am I thinking of? Enter q to give up. ")
+        if guess_amount != 1:
+            print(f"Sorry! That's the wrong number. Try again! ")
+
+        if guess_amount > 1:
+            print(f"You have {guess_amount} guesses remaining.")
+            guess_amount -= 1
+            guess = input("What number am I thinking of? Enter q to give up. ")
+        
+        elif guess_amount == 1:
+            print(f"You have 1 guess remaining. Be careful!")
+            guess_amount -= 1
+            guess = input("What number am I thinking of? Enter q to give up. ")
+
+        else:
+            print(f"Sorry! You've run out of guesses! The number was {number}")
+            again = False
