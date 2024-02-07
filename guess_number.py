f1c1a78c03a3d688cec8f735a766e005ec0114fe
Jonathan Maronde <j2mike4@gmail.com>
2024-02-07 12:17:51 -0600

    Modified guess_number.py to allow the user to either quit or keep trying after wrong guesses

--- guess_number.py
+++ guess_number.py
@@ -5,9 +5,19 @@ Number guessing game
 number = 10
 
 print("I'm thinking of a number...")
-guess = int(input("What number am I thinking of? "))
+guess = input("What number am I thinking of? Enter q to give up. ")
 
-if guess == number:
-   print("Congratulations! You guessed the right number.")
-else:
-   print(f"Sorry! The number was {number}.")
+again = True
+while again:
+
+    if guess == 'q':
+        print(f"Thanks for trying! The number was {number}")
+        again = False
+
+    elif int(guess) == number:
+        print("Congratulations! You guessed the right number.")
+        again = False
+
+    else:
+        print(f"Sorry! That's the wrong number. Try again! ")
+        guess = input("What number am I thinking of? Enter q to give up. ")
